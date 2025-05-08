5ba03d2081e269a07bf7acd703a1bdaad4a1de29
Reevan D Mello <120363587+KingReevan@users.noreply.github.com>
2025-05-08 12:46:36 +0530

    Removed settings features

--- leftPanel.js
+++ leftPanel.js
@@ -103,8 +103,6 @@ async function updateLeftPanel(contentType) {
             categoryPanel.innerHTML = `
                 <button class="category" id="UserProfile" onclick="updateMainContent('UserProfile')">User Profile</button>
                 <button class="category" id="BusinessInfo" onclick="updateMainContent('BusinessInfo')">Business Information</button>
-                <button class="category" id="DisplaySettings" onclick="updateMainContent('DisplaySettings')">Display Settings</button>
-                <button class="category" id="TaxAndDiscount" onclick="updateMainContent('TaxAndDiscount')">Tax and Discounts</button>
                 <button class="category" id="PrinterConfig" onclick="updateMainContent('PrinterConfig')">Printer Configuration</button>
                 <button class="category" id="Receipt" onclick="updateMainContent('Receipt')">Receipt</button>
                 <button class="category" id="ConnectedDevices" onclick="updateMainContent('ConnectedDevices')">Connected Devices</button>
