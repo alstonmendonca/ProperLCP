c193d92963c9d3503ffdaef8125a89e6f05721e0
KingReevan <reevandmellO@gmail.com>
2025-08-26 08:00:51 +0530

    Changed Edit Icon to a professional SVG. Bill printing error logic still saves order to db

--- todaysOrders.js
+++ todaysOrders.js
@@ -94,7 +94,12 @@ ipcRenderer.on("todays-orders-response", (event, data) => {
     orders.forEach(order => {
         gridHTML += `
             <div class="todays-order-box" data-billno="${order.billno}">
-                <div class="todays-orders-edit-button" onclick="event.stopPropagation(); openTodayEditOrder(${order.billno})">✏️</div>
+                <div class="todays-orders-edit-button" onclick="event.stopPropagation(); openTodayEditOrder(${order.billno})">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                    </svg>
+                </div>
                 <h3>Bill No: ${order.billno}</h3>
                 <p>Cashier: ${order.cashier_name}</p>
                 <p>KOT: ${order.kot}</p>
